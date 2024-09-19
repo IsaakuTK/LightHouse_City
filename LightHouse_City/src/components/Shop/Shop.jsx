51f66c0a50beaba71059a1c297b895bf922337fe
IsaakuTK <isaaccallecardozo@gmail.com>
2024-09-18 21:38:53 -0500

    responsive

--- LightHouse_City/src/components/Shop/Shop.jsx
+++ LightHouse_City/src/components/Shop/Shop.jsx
@@ -3,25 +3,26 @@ import { Item } from '../Item/Item';
 
 export function Shop({ closeShop, array }) {
   return (
-    <div className="fixed z-10  w-full h-full flex justify-center items-center">
+    <div className="fixed z-10 inset-0 flex justify-center items-center">
 
-      <div className="absolute z-10 w-full h-full bg-black opacity-80" onClick={closeShop}> </div>
+      {/* Fondo oscuro */}
+      <div className="absolute inset-0 bg-black opacity-80" onClick={closeShop}></div>
 
       {/* Ventana emergente */}
-      <div className="bg-slate-700 p-8 rounded-lg shadow-lg z-20 w-6/12 h-4/6 flex flex-col items-center ">
-        <button onClick={closeShop} className="self-end">X</button>
-        <p className='text-white text-lg font-bold mb-10'>Hi little little thief! Here I have some articles that may interest you.</p>
+      <div className="relative bg-slate-700 p-4 md:p-8 rounded-lg shadow-lg z-20 w-11/12 md:w-8/12 lg:w-6/12 h-5/6 md:h-4/6 flex flex-col items-center">
+        <button onClick={closeShop} className="self-end text-white">X</button>
+        <p className="text-white text-lg font-bold mb-4 md:mb-10 text-center">
+          Hi little thief! Here I have some articles that may interest you.
+        </p>
 
-        <div className='flex flex-col h-full w-full items-center max-h-full overflow-auto'>
-        {array.map((item, index) => (
-              <div
-                key={index}
-                className="px-4 py-2 cursor-pointer w-full">
-                <Item image={item.image} text={item.text} value={item.value}/>
-              </div>
-            ))}
+        {/* Lista de artículos */}
+        <div className="flex flex-col h-full w-full items-center max-h-full overflow-auto">
+          {array.map((item, index) => (
+            <div key={index} className="px-2 md:px-4 py-2 cursor-pointer w-full">
+              <Item image={item.image} text={item.text} value={item.value} />
+            </div>
+          ))}
         </div>
-
       </div>
     </div>
   );
--- LightHouse_City/src/router/AppRouter.jsx
+++ LightHouse_City/src/router/AppRouter.jsx
@@ -5,7 +5,7 @@ import { Dashboard } from '../pages'
 export function AppRouter () {
   return (
     <Routes>
-      <Route path='/' element={<Dashboard />} />
+      <Route path='/LightHouse_City' element={<Dashboard />} />
       <Route path='/*' element={<p className='text-5xl p-5'>404</p>} />
     </Routes>
   )
